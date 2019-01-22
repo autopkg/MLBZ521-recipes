c14089237124fc9174a19648b4305746729b7bb9
Zack T <MLBZ521@gmail.com>
2019-01-22 13:28:47 -0700

    Updated for change in download meta data source
    Literally released a new version today and completely changed the source I was reading.
    Good news:  They pulled all the embed JSON from the HTML page and put it into separate JSON file; WITH actual valid formatting.
    Bad news:  I wasted all that time, regex’ing invalid formatted json into valid formatted json…

--- ARCHICAD/ARCHICADUpdatesProcessor.py
+++ ARCHICAD/ARCHICADUpdatesProcessor.py
@@ -14,10 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
 import requests
-import json
-from HTMLParser import HTMLParser
 from autopkglib import Processor, ProcessorError
 
 __all__ = ["ARCHICADUpdatesProcessor"]
@@ -54,42 +51,17 @@ class ARCHICADUpdatesProcessor(Processor):
 
     def main(self):
 
-        # Define some variables
+        # Define some variables.
         major_version = self.env.get("major_version")
         localization = self.env.get("localization")
         relase_type = self.env.get("relase_type")
-
-        class MyHTMLParser(HTMLParser):
-            # Initializing list
-            lsData = list()
-
-            # HTML Parser Methods
-            def handle_data(self, data):
-                self.lsData.append(data)
-
-        response = requests.get('https://www.graphisoft.com/downloads/archicad/updates/')
-        parser = MyHTMLParser()
-        parser.feed(response.content)
-
-        for data in parser.lsData:
-            json_text = re.search(r'.*\}\(\[.*$', data, flags=re.DOTALL | re.MULTILINE)
-            if json_text:
-                content = data
-
-        content_Split1 = re.split(r'\}\(', content)  # Split contents at:  }(
-        content_Split2 = re.split(r'\);', content_Split1[1])  # Split contents at:  \;
-        clean_Spacing = re.sub(r'^[\s]*', '', content_Split2[0], flags=re.MULTILINE)  # Strip some leading spaces
-        escape_Double_Quotes = re.sub(r'"', '\\"', clean_Spacing, flags=re.MULTILINE)  # Escape existing double quotes
-        replace_Single_Quotes = re.sub(r'\'', '"', escape_Double_Quotes, flags=re.MULTILINE)  # Replace existing single quotes with double quotes
-        braces_New_Lines = re.sub(r'^{\s?\b', '{\n', replace_Single_Quotes, flags=re.MULTILINE)  # At all open braces '{', insert a new line
-        add_New_Lines = re.sub(r'",[^\n]\b', '",\n', braces_New_Lines, flags=re.MULTILINE)  # Add new lines where needed
-        quote_Keys = re.sub(r'^(\w*\b)', r'"\1"', add_New_Lines, flags=re.MULTILINE)  # Quote the keys
-        remove_Extra_Commas = re.sub(r',(\s*(]|}))', r'\1', quote_Keys, flags=re.MULTILINE)  # Remove extra commas
-
-        valid_JSON = json.loads(remove_Extra_Commas)
         available_builds = {}
 
-        for json_Object in valid_JSON:
+        # Grab the available downloads.
+        response = requests.get('https://www.graphisoft.com/downloads/db-v3.json')
+
+        # Parse through the available downloads for versions that match the requested paramters.
+        for json_Object in response.json():
             if json_Object.get('version') == '22':
                 if json_Object.get('localization') == 'USA':
                     if json_Object.get('type') == 'FULL':
@@ -97,6 +69,7 @@ class ARCHICADUpdatesProcessor(Processor):
                             if details.get('platform') == 'mac':
                                 available_builds[json_Object.get('build')] = details['url']
 
+        # Get the latest version.
         build = sorted(available_builds.keys())[-1]
         url = available_builds[build]
 

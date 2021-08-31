005230da4f0173e190956ad858ae08d5abf51b34
Zack T <MLBZ521@gmail.com>
2021-08-30 19:57:54 -0700

    Added support for mounting an SMB share
    Added support for mounting an SMB share within the processor.  Will no longer have to have this share pre-mounted, outside of the AutoPkg run.
    
    (Currently the SMB mount logic assumes `JSSImporter` is installed, if anyone is using this processor and isn't also using `JSSImporter`, let me know and I can adjust the logic to not use `JSSImporter`.)

--- Shared Processors/OfflineApps.py
+++ Shared Processors/OfflineApps.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2020 Zack Thompson (MLBZ521)
+# Copyright 2021 Zack Thompson (MLBZ521)
 #
 # Based on work by:
 #   Jesse Peterson / https://github.com/facebook/Recipes-for-AutoPkg/blob/master/Shared_Processors/SubDirectoryList.py
@@ -25,8 +25,12 @@
 from __future__ import absolute_import
 
 import os
+import sys
 
 from pkg_resources import parse_version
+
+sys.path.insert(0, "/Library/AutoPkg/JSSImporter")
+import jss
 
 from autopkglib import Processor, ProcessorError, URLDownloader
 
@@ -44,39 +48,80 @@ class OfflineApps(URLDownloader):
     input_variables = {
         "search_path": {
             "description": "Root path to search within.",
-            "required": True,
+            "required": True
+        },
+        "OFFLINEAPPS_SMB_URL": {
+            "description": (
+                "An optional SMB URL to mount containing the search path."
+            ),
+            "required": False
+        },
+        "OFFLINEAPPS_SMB_SHARE_NAME": {
+            "description": "Share to mount from the SMB server.",
+            "required": False
+        },
+        "OFFLINEAPPS_SMB_PORT": {
+            "description": (
+                "Port to use to connect to the SMB server.  "
+                "Default port used is 445."
+            ),
+            "default": "445",
+            "required": False
+        },
+        "OFFLINEAPPS_SMB_MOUNT_POINT": {
+            "description": (
+                "Where the SMB share will be mounted too.  "
+                "Default mount point will be '/tmp/OfflineApps/'."
+            ),
+            "default": "/tmp/OfflineApps/",
+            "required": False
+        },
+        "OFFLINEAPPS_SMB_USERNAME": {
+            "description": "Username required to connect to the SMB server.",
+            "required": False
+        },
+        "OFFLINEAPPS_SMB_PASSWORD": {
+            "description": "Password required to connect to the SMB server.",
+            "required": False
+        },
+        "OFFLINEAPPS_SMB_DOMAIN": {
+            "description": "Domain, if required, to connect to the SMB server.",
+            "required": False
         },
         "search_string": {
-            "description": ("String that will be used to match items in the "
-                            "search_path."),
+            "description": (
+                "String that will be used to match items in the search_path."
+            ),
             "required": True,
         },
         "major_version": {
-            "description": ("The major version that will be used to match "
-                            "items in the search_path."),
+            "description": (
+                "The major version that will be used to match "
+                "items in the search_path."
+            ),
             "required": False,
         },
         "exception_string": {
-            "description": ("String will exclude items from matching the "
-                            "search"),
+            "description": "String will exclude items from matching the search",
             "required": False,
         },
         "limitation_string": {
-            "description": ("String that will further limit matching the "
-                            "search"),
+            "description": "String that will further limit matching the search",
             "required": False,
         },
         "version_separator": {
-            "description": ("Character used to separate the "
-                            "\"Software Title Name\" from the \"Version\"."
-                            "For example:  CrowdStrike Falcon-5.27.10803.0"
-                            "The hyphen would be the separator character."
-                            "Defaults to \"-\""),
+            "description": (
+                "Character used to separate the "
+                "\"Software Title Name\" from the \"Version\"."
+                "For example:  CrowdStrike Falcon-5.27.10803.0"
+                "The hyphen would be the separator character."
+                "Defaults to \"-\""
+            ),
             "default": "-",
             "required": False,
         },
         "max_depth": {
-            "description": ("Maximum depth of folders to iterate through."),
+            "description": "Maximum depth of folders to iterate through.",
             "default": "1",
             "required": False,
         },
@@ -93,16 +138,16 @@ class OfflineApps(URLDownloader):
     }
     output_variables = {
         "version": {
-            "description": ("The highest version found according to "
-                            "pkg_resources.parse_version logic.")
+            "description": (
+                "The highest version found according to "
+                "pkg_resources.parse_version logic."
+            )
         },
         "found_major_version": {
-            "description": ("The highest version found according to "
-                            "pkg_resources.parse_version logic.")
+            "description": "The \"major version\" of the version string found."
         },
         "cached_path": {
-            "description": ("Path to the existing contents in the AutoPkg "
-                            "Cache.")
+            "description": "Path to the existing contents in the AutoPkg Cache."
         },
         "download_changed": {
             "description": (
@@ -113,6 +158,27 @@ class OfflineApps(URLDownloader):
     }
 
     description = __doc__
+
+
+    def mount_via_jssimporter(self):
+        """Mount a share via JSSImporter"""
+
+        try:
+            self.dp_instance = jss.distribution_point.SMBDistributionPoint(
+                url = self.env.get("OFFLINEAPPS_SMB_URL"), 
+                share_name = self.env.get("OFFLINEAPPS_SMB_SHARE_NAME"), 
+                port = self.env.get("OFFLINEAPPS_SMB_PORT", "445"), 
+                mount_point = self.env.get("OFFLINEAPPS_SMB_MOUNT_POINT", "/tmp/OfflineApps/"), 
+                username = self.env.get("OFFLINEAPPS_SMB_USERNAME"), 
+                password = self.env.get("OFFLINEAPPS_SMB_PASSWORD"), 
+                domain = self.env.get("OFFLINEAPPS_SMB_DOMAIN"), 
+                jss = jss.JSS(url="/")
+            )
+
+            self.dp_instance.mount()
+
+        except:
+            raise ProcessorError("Unable to mount the SMB share!")
 
 
     def walk(self, top, maxdepth):
@@ -155,6 +221,7 @@ class OfflineApps(URLDownloader):
 
         # Get environment variables
         search_path = self.env.get("search_path")
+        smb_path = self.env.get("smb_path")
         foldername_contains = self.env.get("search_string")
         foldername_not_contain = self.env.get("exception_string")
         foldername_must_contain = self.env.get("limitation_string")
@@ -167,90 +234,101 @@ class OfflineApps(URLDownloader):
 
         # Define local variables
         downloads_dir = os.path.join(RECIPE_CACHE_DIR, "downloads")
-        # search_string = "{0}"
         folder_list = []
 
-        # Verify the directory exists
-        if not os.path.isdir(search_path):
-            raise ProcessorError("Can't find root path or network share not mounted!")
+        # Mount SMB share if passed
+        if self.env.get("OFFLINEAPPS_SMB_URL"):
+            self.mount_via_jssimporter()
 
-        # Build the directory list of the root of the search_path
-        for root_directory, folders, files in self.walk(search_path, int(max_depth)):
+        try:
 
-            # Loop through the folders
-            for foldername in folders:
+            # Verify the directory exists
+            if not os.path.isdir(search_path):
+                raise ProcessorError("Can't find root path or network share not mounted!")
 
-                # If the foldername does not contain a string it should contain, skip it
-                if foldername_contains and (foldername_contains not in foldername):
-                    continue
+            # Build the directory list of the root of the search_path
+            for root_directory, folders, files in self.walk(search_path, int(max_depth)):
 
-                # If the foldername contains a string it should not contain, skip it
-                if foldername_not_contain and (foldername_not_contain in foldername):
-                    continue
+                # Loop through the folders
+                for foldername in folders:
 
-                # If the foldername does not contain a secondary string it should contain, skip it
-                if foldername_must_contain and (foldername_must_contain not in foldername):
-                    continue
+                    # If the foldername does not contain a string it should contain, skip it
+                    if foldername_contains and (foldername_contains not in foldername):
+                        continue
 
-                # Append matching folder to list for future use
-                folder_list.append(os.path.join(root_directory, foldername))
-                self.output("Matched:  {}".format(foldername), verbose_level=2)
+                    # If the foldername contains the string it should not contain, skip it
+                    if foldername_not_contain and (foldername_not_contain in foldername):
+                        continue
 
-        if not folder_list:
-            raise ProcessorError("Was not able to find a match!")
+                    # If the foldername does not contain the secondary 
+                    # string it should contain, skip it
+                    if foldername_must_contain and (foldername_must_contain not in foldername):
+                        continue
 
-        # Get the latest version from the matched results
-        self.env["version"], version_location = self.get_latest_version(
-            folder_list, major_version_like, version_separator)
+                    # Append matching folder to list for future use
+                    folder_list.append(os.path.join(root_directory, foldername))
+                    self.output("Matched:  {}".format(foldername), verbose_level=2)
 
-        if None in { self.env["version"], version_location }:
-            raise ProcessorError("Was not able to match a version!")
+            if not folder_list:
+                raise ProcessorError("Was not able to find a match!")
 
-        self.env["found_major_version"] = (self.env["version"]).split(".", 1)[0]
-        version_folder = (version_location).split("/")[-1]
+            # Get the latest version from the matched results
+            self.env["version"], version_location = self.get_latest_version(
+                folder_list, major_version_like, version_separator)
 
-        self.output("Found major version:  {}".format(
-            self.env["found_major_version"]), verbose_level=2)
-        self.output("Latest version found:  {}".format(self.env["version"]))
-        self.output("Location:  {}".format(version_location), verbose_level=2)
+            if None in { self.env["version"], version_location }:
+                raise ProcessorError("Was not able to match a version!")
 
-        # Set the location where version should be cached
-        self.env["cached_path"] = os.path.join(downloads_dir, version_folder)
+            self.env["found_major_version"] = (self.env["version"]).split(".", 1)[0]
+            version_folder = (version_location).split("/")[-1]
 
-        # Loop over files in version_location and download each one?
-        for root_directory, folders, files in self.walk(version_location, int(0)):
+            self.output("Found major version:  {}".format(
+                self.env["found_major_version"]), verbose_level=2)
+            self.output("Latest version found:  {}".format(self.env["version"]))
+            self.output("Location:  {}".format(version_location), verbose_level=2)
 
-            for file in files:
+            # Set the location where version should be cached
+            self.env["cached_path"] = os.path.join(downloads_dir, version_folder)
 
-                server_path = "{}/{}".format(version_location, file)
-                save_path = "{}/{}".format(self.env["cached_path"], file)
+            # Loop over files in version_location and download each one?
+            for root_directory, folders, files in self.walk(version_location, int(0)):
 
-                if os.path.exists(save_path) and check_filesize and \
-                    os.path.getsize(server_path) == os.path.getsize(save_path):
-                    self.output("File exists locally, not downloading...")
-                    continue
+                for file in files:
 
-                self.output("Downloading file:  {}".format(file))
-                self.output("Path where file will be saved:  {}".format(
-                    save_path), verbose_level=2)
+                    server_path = "{}/{}".format(version_location, file)
+                    save_path = "{}/{}".format(self.env["cached_path"], file)
 
-                # Build the required curl switches
-                curl_opts = [
-                    "--url", "file://{}".format(server_path),
-                    "--request", "GET",
-                    "--output", save_path,
-                    "--create-dirs"
-                ]
+                    if os.path.exists(save_path) and check_filesize and \
+                        os.path.getsize(server_path) == os.path.getsize(save_path):
+                        self.output("File exists locally, not downloading...")
+                        continue
 
-                try:
-                    # Initialize the curl_cmd, add the curl options, and execute the curl
-                    curl_cmd = self.prepare_curl_cmd()
-                    curl_cmd.extend(curl_opts)
-                    self.download_with_curl(curl_cmd)
-                    self.env["download_changed"] = True
+                    self.output("Downloading file:  {}".format(file))
+                    self.output("Path where file will be saved:  {}".format(
+                        save_path), verbose_level=2)
 
-                except:
-                    raise ProcessorError("Failed to download:  {}".format(file))
+                    # Build the required curl switches
+                    curl_opts = [
+                        "--url", "file://{}".format(server_path),
+                        "--request", "GET",
+                        "--output", save_path,
+                        "--create-dirs"
+                    ]
+
+                    try:
+                        # Initialize the curl_cmd, add the curl options, and execute the curl
+                        curl_cmd = self.prepare_curl_cmd()
+                        curl_cmd.extend(curl_opts)
+                        self.download_with_curl(curl_cmd)
+                        self.env["download_changed"] = True
+
+                    except:
+                        raise ProcessorError("Failed to download:  {}".format(file))
+
+        finally:
+            # Done with distribution point, unmount it.
+            if self.dp_instance.is_mounted():
+                self.dp_instance.umount()
 
 
 if __name__ == "__main__":

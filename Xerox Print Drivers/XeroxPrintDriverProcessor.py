8e01116bdf4a855f4b19ff536e51a4a8a89f179c
Zack T <MLBZ521@gmail.com>
2021-09-11 12:21:33 -0700

    Updated to reflect changes to Xerox's website (again)
    * Updated to reflect changes to Xerox's website (again)
    * Improved logic of the `try/except` blocks to improved the accuracy of the verboseness of raised errors
    * Removed duplicate `output`s

--- Xerox Print Drivers/XeroxPrintDriverProcessor.py
+++ Xerox Print Drivers/XeroxPrintDriverProcessor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2019 Zack T (mlbz521)
+# Copyright 2021 Zack T (mlbz521)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,13 +16,16 @@
 
 from __future__ import absolute_import, print_function
 
-from html.parser import HTMLParser
 import json
 import re
 
+from html.parser import HTMLParser
+
 from autopkglib import Processor, ProcessorError, URLGetter
 
+
 __all__ = ["XeroxPrintDriverProcessor"]
+
 
 class XeroxPrintDriverProcessor(URLGetter):
     """This processor finds the URL for the desired Xerox print driver version.
@@ -60,6 +63,7 @@ class XeroxPrintDriverProcessor(URLGetter):
 
     description = __doc__
 
+
     def main(self):
         """Main process."""
 
@@ -84,8 +88,8 @@ class XeroxPrintDriverProcessor(URLGetter):
                         # Loop back through and get the 'href' path
                         for name, value in attributes:
 
-                            if name == 'href':
-                                self.url_path = value
+                            if name == 'onclick':
+                                self.url_path = re.search("https:\/\/.*[.]\w\w\w", value)[0]
                                 return
 
 
@@ -110,54 +114,54 @@ class XeroxPrintDriverProcessor(URLGetter):
         ]
 
         try:
-            # Initialize the curl_cmd, add the curl options, and execute the curl
+            # Initialize the curl_cmd, add the curl options, and execute curl
             curl_cmd = self.prepare_curl_cmd()
             self.add_curl_headers(curl_cmd, headers)
             curl_cmd.extend(curl_opts)
-            response_token = self.download_with_curl(curl_cmd)
+            model_lookup = self.download_with_curl(curl_cmd)
+
+            self.output("Model lookup response:  {}".format(model_lookup), verbose_level=4)
 
         except:
             raise ProcessorError("Failed to match the provided model:  {}".format(input_model))
 
-
         try:
             # Load the JSON Response
-            json_data = json.loads(response_token)
+            json_data = json.loads(model_lookup)
 
             for result in json_data['results']:
                 if re.match(r".*Drivers & Downloads", result.get('title')):
-                    downloads_uri = result.get('clickUri')
-                    # self.output("downloads_uri:  {}".format(downloads_uri))
+                    model_downloads_page = result.get('clickUri')
+                    self.output("Model downloads page:  {}".format(model_downloads_page), verbose_level=2)
 
         except:
             raise ProcessorError("Failed to find a model url in the results!")
 
-        try:
-            # Build url
-            lookupURL = '{downloads_uri}?platform=macOS{osVersion}'.format(downloads_uri=downloads_uri, osVersion=osVersion)
-            # self.output("lookupURL:  {}".format(lookupURL))
+        # Build url
+        os_version_lookup_url = '{}?platform=macOS{}'.format(model_downloads_page, osVersion)
+        self.output("OS version lookup URL:  {}".format(os_version_lookup_url), verbose_level=2)
 
-            # Perform second lookup to get available download types
-            pageContent = self.download(lookupURL, text=True)
+        try:
+
+            # Perform lookup to get available download types
+            pageContent = self.download(os_version_lookup_url, text=True)
+            self.output("Page Content:  {}".format(pageContent), verbose_level=4)
 
             # Parse the HTML for the desired data
             parser.feed(pageContent)
-
             download_path = parser.url_path
-            # print("download_path:  {}".format(download_path))
-
-            if download_path:
-                # url = "https://www.support.xerox.com{}".format(download_path)
-
-                # Return results
-                self.env["url"] = download_path
-                self.output("Download URL: {}".format(self.env["url"]))
-
-            else:
-                raise ProcessorError("Unable to build a download url based on the parameters provided.")
 
         except:
-            raise ProcessorError("Unable to build a download url based on the parameters provided.")
+            raise ProcessorError("Unable to find available downloads or download types.")
+
+        if download_path:
+            # Return results
+            self.env["url"] = download_path
+            self.output("Download URL: {}".format(self.env["url"]))
+
+        else:
+            raise ProcessorError("Failed to find a matching download type for the provided model.")
+
 
 if __name__ == "__main__":
     processor = XeroxPrintDriverProcessor()

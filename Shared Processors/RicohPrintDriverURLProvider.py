ed005a3a6187ee754699b389f5dcabaae7b7b8e6
Zack T <MLBZ521@gmail.com>
2022-12-15 17:33:11 -0700

    Updates to support changes to Ricoh's website
    * Use the Ricoh API to lookup models and obtain driver page; this simplifies the first part of the processor and should make it more reliable
    * Add support for Ventura, making it the default

--- Shared Processors/RicohPrintDriverURLProvider.py
+++ Shared Processors/RicohPrintDriverURLProvider.py
@@ -14,11 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
 import os
 import sys
-
-from html.parser import HTMLParser
+import json
 
 from autopkglib import ProcessorError, URLGetter
 
@@ -50,18 +48,16 @@ class RicohPrintDriverURLProvider(URLGetter):
         },
         "os_version": {
             "required": False,
-            "description": (
-                "The OS version to search against.",
-                "Default:  'Big Sur"
-            )
+            "description": "The OS version to search against.",
+            "default":  "latest"
         },
         "web_driver": {
             "required": False,
             "description": (
                 "The web driver engine to use.  Only Chrome is supported at this time, "
-                "but support for additional web drivers can be added.",
-                "Default:  Chrome"
-            )
+                "but support for additional web drivers can be added."
+            ),
+            "default":  "Chrome"
         },
         "web_driver_path": {
             "required": False,
@@ -83,51 +79,25 @@ class RicohPrintDriverURLProvider(URLGetter):
             "description": "Returns the url to download."
         }
     }
-
     description = __doc__
-
-
-    class MyHTMLParser(HTMLParser):
-
-        def __init__(self):
-            HTMLParser.__init__(self)
-            self.url_path = ""
-            self.search_match = ""
-
-        def handle_starttag(self, tag, attributes):
-
-            if not self.url_path and tag == "a":
-
-                for name, value in attributes:
-
-                    if name == 'onclick':
-
-                        self.search_match = re.search(
-                            r".*(https:\/\/support[.]ricoh[.]com/bb/html/.+[.]htm)", value)
-
-                        if self.search_match:
-
-                            self.url_path = self.search_match.group(1)
-                            break
 
 
     def main(self):
         """Do the main thing."""
 
         # Define variables
-        model = self.env.get('model')
+        if not (model := self.env.get('model')):
+            raise ProcessorError(
+                "Expected an 'model' input variable but one was not set!")        
         os_version = self.env.get("os_version", "latest")
         web_driver = self.env.get("web_driver", "Chrome")
         web_driver_path = self.env.get("web_driver_path")
         web_driver_binary_location = self.env.get("web_driver_binary_location")
-
-        if not model:
-            raise ProcessorError(
-                "Expected an 'model' input variable but one was not set!")
-
         self.output(f'Searching for printer model:  {model}')
 
-        if os_version in ["Monterey", "latest", "", None]:
+        if os_version in ["Ventura", "latest", "", None]:
+            os_version_keycode="122825"
+        elif os_version == "Monterey":
             os_version_keycode="121887"
         elif os_version == "Big Sur":
             os_version_keycode="120905"
@@ -142,37 +112,41 @@ class RicohPrintDriverURLProvider(URLGetter):
         else:
             raise ProcessorError("Unknown OS Version requested.")
 
-        # Build the URL
-        model_escaped = re.sub(r"\s", "_", model)
-        lookupURL = f"https://www.ricoh-usa.com/Common/DownloadSearch/LoadSearchDetails?searchfor={model_escaped}&isBrochure=true&isDrivers=true&isManual=true&endecaURL=DownloadSearch&endecaSearchPage=SearchResultPage&hdnmobiledriverlink=&ismobile=false&hdnEmptyResultText=No+results+found.&hdnEmptySearchResultText1=Your+search+did+not+yield+any+results.&hdnEmptySearchResultText2=did+not+return+any+models.&CategorySelect=Category&SubCategorySelect=Sub+Category&ModelSelect=Model&DriverLabel=DRIVER&DriversLabel=DRIVERS&ManualsLabel=MANUAL"
+        # Build the required curl switches
+        curl_opts = [
+            "--url", "https://www.ricoh-usa.com/api/search",
+            "--data-raw",
+            json.dumps(
+                {"requestState":
+                    {
+                        "current":1,"filters":[],"resultsPerPage":100,"searchTerm":model,
+                        "sortDirection":"","sortField":"","sortList":[]
+                    },
+                    "queryConfig":{
+                        "result_fields":
+                            {"Gen_Linked_Driver_Drivers":{"raw":{}}}
+                    }
+                }
+            )
+        ]
+        headers = {"content-type": "application/json"}
+        try:
+            # Initialize the curl_cmd, add the curl options, and execute curl
+            curl_cmd = self.prepare_curl_cmd()
+            self.add_curl_headers(curl_cmd, headers)
+            curl_cmd.extend(curl_opts)
+            result = self.download_with_curl(curl_cmd)
+        except:
+            raise ProcessorError("Failed to query the Ricoh API.")
 
-        # Look up the product
-        response = self.download(lookupURL)
-
-        # Encode/decode the response into valid HTML
-        html = (response).decode("utf-8").encode().decode('unicode-escape')
-
-        if html:
-
-            try:
-                # Parse the HTML for the desired data
-                parser = self.MyHTMLParser()
-                parser.feed(html)
-
-                if not parser.url_path:
-                    raise ProcessorError("Unable to find available identify the driver download page.")
-
-                self.output(f'Driver download page URL:  {parser.url_path}')
-
-            except:
-                raise ProcessorError("Unable to find available downloads.")
+        drive_page_url = json.loads(
+            result).get("results")[0].get("Gen_Linked_Driver_Drivers").get("raw")
 
         with WebEngine(
             engine=web_driver, binary=web_driver_binary_location, path=web_driver_path, parent=self) as web_engine:
 
             try:
-                web_engine.get(parser.url_path)
-
+                web_engine.get(drive_page_url)
             except:
                 raise ProcessorError("Failed to access the driver download page.")
 
@@ -184,7 +158,6 @@ class RicohPrintDriverURLProvider(URLGetter):
                 )
                 web_engine.find_element(By.XPATH, section_MacOSX).click()
                 self.output("Selected operating system section labeled \"Mac OS X\"", verbose_level=3)
-
             except:
                 raise ProcessorError("Failed to find and open the operating system section labeled \"Mac OS X\".")
 
@@ -196,12 +169,9 @@ class RicohPrintDriverURLProvider(URLGetter):
                     lambda d: d.find_element(By.XPATH, xpath_os_version)
                 )
                 presence_of_element_located(xpath_os_version)
-
                 section_selected_os_version = web_engine.find_element(By.XPATH, xpath_os_version)
                 section_selected_os_version.click
-
                 self.output(f"Selected the section for OS Version:  {os_version}", verbose_level=3)
-
             except:
                 raise ProcessorError("Failed to select the desired OS Version")
 
@@ -210,7 +180,6 @@ class RicohPrintDriverURLProvider(URLGetter):
                 download_url = section_selected_os_version.find_element(By.XPATH, 
                     '//*[@id="os-driver-list"]/div[4]/div/div/div[2]/div[1]/div/div/div[2]/div/div/div/div/div/div[1]/p[2]/a').get_attribute("href"
                 )
-
             except:
                 raise ProcessorError(
                     "Failed to find and collect the download url from the download button.")
@@ -218,7 +187,6 @@ class RicohPrintDriverURLProvider(URLGetter):
         if not download_url:
             raise ProcessorError("Failed to find a matching download type for the provided model.")
 
-        # Return results
         self.env["url"] = download_url
         self.output(f'Download URL: {self.env["url"]}', verbose_level=1)
 
